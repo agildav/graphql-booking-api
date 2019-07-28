f993b415332b527bf73877e5eb8d25a43df8a3ce
Alberto A. Gil D <agildav@gmail.com>
2019-07-28 19:52:36 -0400

    Add types, query and return

--- src/setup/graphql.ts
+++ src/setup/graphql.ts
@@ -2,35 +2,86 @@ import { Application } from "express";
 import graphqlHttp from "express-graphql";
 import { buildSchema } from "graphql";
 
-export function graphqlInit(app: Application) {
-  console.log(":: Setting GraphQL");
-  app.use(
-    "/graphql",
-    graphqlHttp({
-      schema: buildSchema(`
-        type RootQuery {
-          events: [String!]!
-        }
+// Receive these props from user
+interface EventInput {
+  title: string;
+  description: string;
+  price: number;
+}
 
-        type RootMutation {
-          createEvent(name: String): String
-        }
+// Auto generate these props
+interface Event extends EventInput {
+  _id: string;
+  date: string;
+}
 
-        schema {
-          query: RootQuery
-          mutation: RootMutation
-        }
-      `),
-      rootValue: {
-        events: () => {
-          return ["Romantic Cooking", "Sailing"];
+export default class Graphql {
+  private events: [Event];
+
+  constructor() {
+    this.events = [
+      {
+        _id: Math.random().toString(),
+        title: "title one",
+        description: "description one",
+        price: Math.random(),
+        date: new Date().toISOString()
+      }
+    ];
+  }
+
+  graphqlInit(app: Application) {
+    console.log(":: Setting GraphQL");
+    app.use(
+      "/graphql",
+      graphqlHttp({
+        schema: buildSchema(`
+          type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+          }
+
+          input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+          }
+
+          type RootQuery {
+            events: [Event!]!
+          }
+
+          type RootMutation {
+            createEvent(eventInput: EventInput): ID
+          }
+
+          schema {
+            query: RootQuery
+            mutation: RootMutation
+          }
+        `),
+        rootValue: {
+          events: (): [Event] => {
+            return this.events;
+          },
+          createEvent: (e: { eventInput: EventInput }): string => {
+            const newEvent = {
+              _id: Math.random().toString(),
+              title: e.eventInput.title,
+              description: e.eventInput.description,
+              price: e.eventInput.price,
+              date: new Date().toISOString()
+            };
+
+            this.events.push(newEvent);
+            return newEvent._id;
+          }
         },
-        createEvent: args => {
-          const eventName = args.name;
-          return eventName;
-        }
-      },
-      graphiql: true
-    })
-  );
+        graphiql: true
+      })
+    );
+  }
 }
--- src/setup/router.ts
+++ src/setup/router.ts
@@ -1,11 +1,12 @@
 import { Application } from "express";
-import { graphqlInit } from "./graphql";
+import Graphql from "./graphql";
 
 export async function init(app: Application): Promise<any> {
   return new Promise(async (resolve, reject) => {
     try {
       console.log(":: Starting router");
-      await graphqlInit(app);
+
+      await new Graphql().graphqlInit(app);
 
       console.log(":: Router, ready");
       resolve();
